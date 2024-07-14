185ff60a0e89d5099b587d041f7ff5d1fcf5c23c
Marlon Munoz <marlon@marlonshade.com>
2024-07-14 05:42:23 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+
+const set = [5, 10, 20, 15, 8];
+let max = set[0];
+
+for (let i = 1; i < set.length; i++) {
+  if (set[i] > max) {
+ max = set[i];
+  }
+}
